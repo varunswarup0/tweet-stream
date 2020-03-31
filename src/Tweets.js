648a5ee69ea2c5a8440bc642d1d0f8cccab3ad21
Varun Swarup <varunswarup0@gmail.com>
2020-03-31 13:48:01 +0530

    Redux Thunk SetUp

--- src/Tweets.js
+++ src/Tweets.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import Tweet from './Tweet';
+import { connect } from 'react-redux';
 
 const Tweets = ({ tweets = [] }) => {
   return (
     <section className="Tweets">
-      {tweets.map((tweet) => (
+      {tweets.map(tweet => (
         <Tweet key={tweet.id} tweet={tweet} />
       ))}
     </section>
   );
 };
 
-export default Tweets;
+const mapStateToProps = ({ tweets }) => ({ tweets });
+
+export default connect(mapStateToProps)(Tweet);
